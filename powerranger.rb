57919beea50f9c66a657b7c4f7fc1dfc53f8c75d
Randy Davis <randy_davis@outlook.com>
2014-06-29 00:12:59 -0500

    Initial commit.  Create person class.

--- powerranger.rb
+++ powerranger.rb
@@ -1,4 +1,23 @@
 class Person
+	attr_accessor :name, :caffeine_level
+	def initialize(name, caffeine_level=5)
+		@name = name
+		@caffeine_level = caffeine_level
+	end
+
+	def run
+		@caffeine_level -=1
+		puts "After running, your current caffeine_level is at #{@caffeine_level}."
+	end
+
+	def scream
+		puts "ARRRRRGH!"
+	end
+
+	def drink_coffee(cups)
+		@caffeine_level += 1
+		puts "You increased your caffeine_level by #{cups}. Caffeine level is at #{@caffeine_level}."
+	end
 end
 
 class PowerRanger
@@ -6,3 +25,6 @@ end
 
 class EvilNinja
 end
+
+ted = Person.new("Ted")
+ted.drink_coffee(1)
