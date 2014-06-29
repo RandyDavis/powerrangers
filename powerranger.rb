073975dfdeb52d60aa758cf5cd7a615aabeb3025
Randy Davis <randy_davis@outlook.com>
2014-06-29 17:20:40 -0500

    Update various person methods and add fight scene method.

--- powerranger.rb
+++ powerranger.rb
@@ -8,7 +8,7 @@ class Person
 	def run
 		if @caffeine_level > 5
 			@caffeine_level -=5
-			puts "#{name} has run away to safety. After running, #{name}'s current caffeine_level is at #{@caffeine_level}."
+			puts "#{name} has run away to safety. After running, #{name}'s current caffeine level is at #{@caffeine_level}."
 		else
 			@caffeine_level < 1
 			puts "#{name} has no energy left to retreat and has been defeated."
@@ -21,7 +21,7 @@ class Person
 
 	def drink_coffee(cups)
 		@caffeine_level += 1
-		puts "You increased your caffeine_level by #{cups}. Caffeine level is at #{@caffeine_level}."
+		puts "#{name} increased their caffeine_level by #{cups}. Caffeine level is at #{caffeine_level}."
 	end
 end
 
@@ -37,9 +37,11 @@ class PowerRanger < Person
 	def punch(person)
 		if @strength > 5
 			puts "#{person.name} was somersaulted into the air!"
-			@caffeine_level -=5
+			person.caffeine_level -=5
+			@caffeine_level -=2
 		else
 			puts "#{person.name} caught a glancing blow."
+			person.caffeine_level -=2
 			@caffeine_level -=1
 		end
 		person.run
@@ -49,11 +51,12 @@ class PowerRanger < Person
 	def use_megazord(person)
 		@strength = 5000
 		if @strength > 4999
-			puts "#{person.name} was thrown through a building, caught in midair on the otherside, thrown even higher into the air, caught just before entering space, and punched down to the ground with such force that created a 6 mile wide crater!"
+			person.caffeine_level =0
+			puts "#{person.name} was thrown through a building, caught in midair on the otherside, thrown even higher into the air, caught just before entering space, and punched down to the ground with such force that created a 6 mile wide crater! The #{person.name} has been defeated."
 			@caffeine_level -=50
 		end
 		person.scream
-		puts "The #{name}'s current caffeine level is now #{@caffeine_level}."
+		puts "The #{name}'s current caffeine level is now #{@caffeine_level}.  #{person.name} has been defeated."
 	end
 end
 
@@ -67,10 +70,12 @@ class EvilNinja < Person
 
 	def punch(person)
 		if @strength > 5
-			puts "#{person.name} was punched very hard."
-			@caffeine_level -=5
+			puts "#{person.name} was punched very hard by #{name}."
+			person.caffeine_level -=5
+			@caffeine_level -=2
 		else
-			puts "#{person.name} caught a glancing blow."
+			puts "#{person.name} caught a glancing blow from #{name}."
+			person.caffeine_level -=2
 			@caffeine_level -=1
 		end
 		person.run
@@ -85,14 +90,27 @@ class EvilNinja < Person
 	end
 end
 
-ted = Person.new("Ted")
-rick = Person.new("Rick", 60)
-puts "----------------------------"
-red_ranger = PowerRanger.new("Red Ranger", "red", 100, 100)
-red_ranger.punch(ted)
-red_ranger.use_megazord(ted)
-red_ranger.punch(ted)
-puts "----------------------------"
-gray_ninja = EvilNinja.new("Gray Assassin", 100, 100, 100)
-gray_ninja.punch(rick)
-gray_ninja.cause_mayhem(rick)
+def fight_scene
+	ted = Person.new("Ted", 50)
+	rick = Person.new("Rick", 50)
+	puts "----------------------------"
+	red_ranger = PowerRanger.new("Red Ranger", "red", 100, 100)
+	blue_ranger = PowerRanger.new("Blue Ranger", "blue", 100, 100)
+	puts "----------------------------"
+	gray_ninja = EvilNinja.new("Gray Assassin", 100, 100, 100)
+	green_ninja = EvilNinja.new("Green Assassin", 100, 100, 100)
+	puts "----------------------------"
+	gray_ninja.punch(rick)
+	red_ranger.punch(gray_ninja)
+	blue_ranger.punch(green_ninja)
+	green_ninja.punch(ted)
+	ted.run
+	ted.drink_coffee(10)
+	gray_ninja.cause_mayhem(rick)
+	red_ranger.use_megazord(gray_ninja)
+	green_ninja.run
+	red_ranger.drink_coffee(20)
+	blue_ranger.drink_coffee(5)
+end
+
+fight_scene
