559187b209c048075754590806e6240b216443c4
Randy Davis <randy_davis@outlook.com>
2014-06-29 16:08:52 -0500

    Add EvilNinja class and updates to  person run method and power ranger punch method.

--- powerranger.rb
+++ powerranger.rb
@@ -8,7 +8,7 @@ class Person
 	def run
 		if @caffeine_level > 5
 			@caffeine_level -=5
-			puts "#{name} has away to safety. After running, #{name}'s current caffeine_level is at #{@caffeine_level}."
+			puts "#{name} has run away to safety. After running, #{name}'s current caffeine_level is at #{@caffeine_level}."
 		else
 			@caffeine_level < 1
 			puts "#{name} has no energy left to retreat and has been defeated."
@@ -38,6 +38,9 @@ class PowerRanger < Person
 		if @strength > 5
 			puts "#{person.name} was somersaulted into the air!"
 			@caffeine_level -=5
+		else
+			puts "#{person.name} caught a glancing blow."
+			@caffeine_level -=1
 		end
 		person.run
 		puts "The #{name}'s current caffeine level is now #{@caffeine_level}."
@@ -54,12 +57,42 @@ class PowerRanger < Person
 	end
 end
 
-class EvilNinja
+class EvilNinja < Person
+	attr_accessor :name, :evilness, :strength, :caffeine_level
+	def initialize(name, evilness=0, strength=0, caffeine_level=0)
+		super(name, caffeine_level)
+		@evilness = evilness
+		@strength = strength
+	end
+
+	def punch(person)
+		if @strength > 5
+			puts "#{person.name} was punched very hard."
+			@caffeine_level -=5
+		else
+			puts "#{person.name} caught a glancing blow."
+			@caffeine_level -=1
+		end
+		person.run
+		puts "The #{name}'s current caffeine level is now #{@caffeine_level}."
+	end
+
+	def cause_mayhem(person)
+		person.caffeine_level = 0
+		puts "#{person.name} was hit by mayhem, causing his energy to drop to #{person.caffeine_level}. #{person.name} has been defeated!"
+		@caffeine_level -=75
+		puts "#{name}'s caffeine level is at #{@caffeine_level} after causing mayhem."
+	end
 end
 
 ted = Person.new("Ted")
+rick = Person.new("Rick", 60)
 puts "----------------------------"
 red_ranger = PowerRanger.new("Red Ranger", "red", 100, 100)
 red_ranger.punch(ted)
 red_ranger.use_megazord(ted)
 red_ranger.punch(ted)
+puts "----------------------------"
+gray_ninja = EvilNinja.new("Gray Assassin", 100, 100, 100)
+gray_ninja.punch(rick)
+gray_ninja.cause_mayhem(rick)
